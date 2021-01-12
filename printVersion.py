85d824fb34e697fd2a95e71f0cf424848ba27abe
PENGYUXIONG <pxiong@ualberta.ca>
2021-01-11 17:07:29 -0700

    Add in answer txt

--- printVersion.py
+++ printVersion.py
@@ -3,5 +3,5 @@ print('The current requests library version is: ', requests.__version__)
 
 #var = requests.get('http://www.google.com')
 
-var = requests.get("https://raw.githubusercontent.com/PENGYUXIONG/cmput404lab1/master/requests_liobrary_version.py")
+var = requests.get("https://raw.githubusercontent.com/PENGYUXIONG/cmput404lab1/master/printVersion.py")
 print(var.content)
